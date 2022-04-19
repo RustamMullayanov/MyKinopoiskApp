20950c84d1f30bbaf98f369eb6d162c995471c20
MullayanovRustam <rustam.mullayanov@gmail.com>
2022-04-20 00:17:08 +0500

    Доработал MoviesAdapter

--- app/src/main/java/com/example/mykinopoiskapp/ui/movies/MoviesAdapter.kt
+++ app/src/main/java/com/example/mykinopoiskapp/ui/movies/MoviesAdapter.kt
@@ -1,25 +1,39 @@
 package com.example.mykinopoiskapp.ui.movies
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mykinopoiskapp.databinding.CardMovieBinding
+import com.example.mykinopoiskapp.domain.entities.Movie
 
 class MoviesAdapter : RecyclerView.Adapter<MoviesAdapter.MoviesHolder>() {
+    private var movies: List<Movie> = emptyList()
 
     class MoviesHolder(val viewBinding: CardMovieBinding) :
         RecyclerView.ViewHolder(viewBinding.root)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MoviesAdapter.MoviesHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MoviesHolder {
         val binding = CardMovieBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return MoviesHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: MoviesAdapter.MoviesHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: MoviesHolder, position: Int) {
+        val item = movies[position]
+        with(holder.viewBinding) {
+            movieFieldName.text = item.title
+            movieFieldGenres.text = item.genres.toString()
+            movieFieldYear.text = item.year.toString()
+        }
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return movies.size
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setMovies(movies: List<Movie>) {
+        this.movies = movies
+        notifyDataSetChanged()
     }
 }
