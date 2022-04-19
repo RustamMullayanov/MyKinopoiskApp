a3c10e7a3c9be9c2ff2bc4786b97c358f1055d13
MullayanovRustam <rustam.mullayanov@gmail.com>
2022-04-19 20:57:07 +0500

    инициализация репозитория

---
+++ app/src/main/java/com/example/mykinopoiskapp/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,30 @@
+package com.example.mykinopoiskapp.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.mykinopoiskapp.databinding.FragmentDashboardBinding
+
+class DashboardFragment : Fragment() {
+
+    private var _binding: FragmentDashboardBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+
+
+        return root
+    }
+}
---
+++ app/src/main/java/com/example/mykinopoiskapp/ui/notifications/NotificationsFragment.kt
@@ -0,0 +1,27 @@
+package com.example.mykinopoiskapp.ui.notifications
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.mykinopoiskapp.databinding.FragmentNotificationsBinding
+
+class NotificationsFragment : Fragment() {
+    private var _binding: FragmentNotificationsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+}
